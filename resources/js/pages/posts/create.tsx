ccf2edcfb6df2d33df9973f272b3f81a5551b846
vickyalvandob <73622790+vickyalvandob@users.noreply.github.com>
2026-01-12 10:50:02 +0700

    add error validation

--- resources/js/pages/posts/create.tsx
+++ resources/js/pages/posts/create.tsx
@@ -1,4 +1,5 @@
 import AppLayout from "@/layouts/app-layout";
+import { cn } from "@/lib/utils";
 import { Form } from "@inertiajs/react";
 
 export default function PostsCreate() {
@@ -7,31 +8,47 @@ export default function PostsCreate() {
       <div>
         <h1 className="text-2xl font-bold text-gray-900 mb-4">Create Post</h1>
         <Form action={'/posts'} method="post" className="space-y-4" >
-          <div>
-            <label htmlFor="title" className="block mb-1">Title</label>
-            <input 
-              id="title" 
-              name="title" 
-              type="text"
-              required
-              className="w-full border rounded px-3 py-2"
-            />
-          </div>
-          <div>
-            <label htmlFor="body" className="block mb-1">Body</label>
-            <textarea 
-              id="body" 
-              name="body" 
-              required
-              className="w-full border rounded px-3 py-2"
-            />
-          </div>
-          <button 
-            type="submit"
-            className="bg-blue-600 text-white px-4 py-2 rounded"
-          >
-            Create
-          </button>
+          {
+            ({errors}) => (<>
+              <div>
+                <label htmlFor="title" className="block mb-1">Title</label>
+                <input 
+                  id="title" 
+                  name="title" 
+                  type="text"
+                  required
+                  className={cn(
+                    "w-full border rounded px-3 py-2",
+                    errors.title && "border-red-500"
+                  )}
+                />
+                {errors.title && (
+                  <p className="text-red-500 text-sm mt-1">{errors.title}</p>
+                )}
+              </div>
+              <div>
+                <label htmlFor="body" className="block mb-1">Body</label>
+                <textarea 
+                  id="body" 
+                  name="body" 
+                  required
+                  className={cn(
+                    "w-full border rounded px-3 py-2",
+                    errors.body && "border-red-500"
+                  )}
+                />
+                {errors.body && (
+                  <p className="text-red-500 text-sm mt-1">{errors.body}</p>
+                )}
+              </div>
+              <button 
+                type="submit"
+                className="bg-blue-600 text-white px-4 py-2 rounded"
+              >
+                Create
+              </button>
+          </>)
+          }
         </Form>
       </div>
     </AppLayout>
